a4637ea109b1767b013b19ef156cf4fa5206dc44
Xavier Malone <xmalone0920@gmail.com>
2023-09-01 14:17:30 -0700

    computer move animation

--- index.js
+++ index.js
@@ -1,196 +1,221 @@
-document.addEventListener('DOMContentLoaded', () => {
-const cells = document.querySelectorAll('.cell'); 
-const playerOneScore = document.querySelector('.playerScore');
-const computerScore = document.querySelector('.computerScore');
-  
-let currentPlayer = 'X'; // playerOne Symbol
-  
-//gameState
-const spaces = ['', '', '', '', '', '', '', '', ''];
-  
-//eventListeners for Board
-cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => {
-    if (!cell.textContent && !checkWin() && !checkDraw()) {
+document.addEventListener("DOMContentLoaded", () => {
+  const cells = document.querySelectorAll(".cell");
+  const playerOneScore = document.querySelector(".playerScore");
+  const computerScore = document.querySelector(".computerScore");
+
+  let currentPlayer = "X"; // playerOne Symbol
+
+  //gameState
+  const spaces = ["", "", "", "", "", "", "", "", ""];
+
+  //eventListeners for Board
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      if (!cell.textContent && !checkWin() && !checkDraw()) {
         cell.textContent = currentPlayer;
         spaces[index] = currentPlayer;
         if (checkWin()) {
-            if (currentPlayer === 'X') {
-                const playerOneWins = +playerOneScore.textContent.split(' ')[0] + 1;
-                playerOneScore.textContent = `${playerOneWins} Wins`;
-                alert('Player 1 (X) wins!');
-            } else {
-                const computerWins = +computerScore.textContent.split(' ')[0] + 1;
-                computerScore.textContent = `${computerWins} Wins`;
-                alert('Player 2 (O) wins!');
-            }
-        resetGame();
+          if (currentPlayer === "X") {
+            const playerOneWins = +playerOneScore.textContent.split(" ")[0] + 1;
+            playerOneScore.textContent = `${playerOneWins} Wins`;
+            alert("Player 1 (X) wins!");
+          } else {
+            const computerWins = +computerScore.textContent.split(" ")[0] + 1;
+            computerScore.textContent = `${computerWins} Wins`;
+            alert("Player 2 (O) wins!");
+          }
+          resetGame();
         } else if (checkDraw()) {
-        alert('It\'s a draw!');
-        resetGame();
+          alert("It's a draw!");
+          resetGame();
         } else {
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
-            if (currentPlayer === 'O') {
-                setTimeout(() => {
-                    makeAIMoveMinimax();
-                    const winner = checkWinner(spaces);
-                    if (winner === 'O') {
-                        computerScore.textContent = parseInt(computerScore.textContent) + 1;
-                        alert('Player 2 (O) wins!');
-                        resetGame();
-                        }
-                    }, 1000);
-                }
-            }
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+          if (currentPlayer === "O") {
+            setTimeout(() => {
+              makeAIMoveMinimax();
+              const winner = checkWinner(spaces);
+              if (winner === "O") {
+                const computerWinsText = computerScore.textContent;
+                const computerWins = parseInt(computerWinsText.match(/\d+/)[0]);
+                computerScore.textContent = `${computerWins + 1} Wins`;
+                alert("Player 2 (O) wins!");
+                resetGame();
+              }
+            }, 1000);
+          }
         }
+      }
     });
-});
-  
-function checkWin() {
+  });
+
+  function checkWin() {
     //check rows
-  for (let i = 0; i < 3; i++) {
-  if (
-    spaces [i*3]!==''&&
-    spaces[i*3]===spaces[i*3+1]&&
-    spaces[i*3]===spaces[i*3+2]
-    ){
-      return true;
+    for (let i = 0; i < 3; i++) {
+      if (
+        spaces[i * 3] !== "" &&
+        spaces[i * 3] === spaces[i * 3 + 1] &&
+        spaces[i * 3] === spaces[i * 3 + 2]
+      ) {
+        return true;
+      }
     }
-  }
-  //check columns
-  for (let i = 0; i < 3; i++){
+    //check columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        spaces[i] !== "" &&
+        spaces[i] === spaces[i + 3] &&
+        spaces[i] === spaces[i + 6]
+      ) {
+        return true;
+      }
+    }
+    //check diagonals
     if (
-    spaces[i]!==''&&
-    spaces[i]===spaces[i+3]&&
-    spaces[i]===spaces[i+6]
-    ){
-    return true;
-    }
-  }
-  //check diagonals
-  if (
-    spaces[0]!==''&&
-    spaces[0]===spaces[4]&&
-    spaces[0]===spaces[8]
-  ){
-    return true;
-  }
-  if (
-    spaces[2]!==''&&
-    spaces[2]===spaces[4]&&
-    spaces[2]===spaces[6]
-    ){
+      spaces[0] !== "" &&
+      spaces[0] === spaces[4] &&
+      spaces[0] === spaces[8]
+    ) {
       return true;
     }
-      return false; //no win condition
+    if (
+      spaces[2] !== "" &&
+      spaces[2] === spaces[4] &&
+      spaces[2] === spaces[6]
+    ) {
+      return true;
+    }
+    return false; //no win condition
   }
+
   //Check draw
-  function checkDraw(){
-    return spaces.every(cell => cell !=='');
+  function checkDraw() {
+    return spaces.every((cell) => cell !== "");
   }
+
   // reset board
-  function resetGame(){
-    cells.forEach(cell => {
-      cell.textContent='';
+  function resetGame() {
+    cells.forEach((cell) => {
+      cell.textContent = "";
     });
-    spaces.fill('');
+    spaces.fill("");
   }
+
   //start new game
   function newGame() {
     resetGame();
-    playerOneScore.textContent = '0'; // Reset scores
-    computerScore.textContent = '0';
+    playerOneScore.textContent = "0"; // Reset scores
+    computerScore.textContent = "0";
   }
-  
+
   function makeAIMoveMinimax() {
-      const bestMove = findBestMove(spaces, currentPlayer);
-      if (bestMove !== -1) {
-        spaces[bestMove] = currentPlayer;
-        cells[bestMove].textContent = currentPlayer;
-        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+    const bestMove = findBestMove(spaces, currentPlayer);
+    if (bestMove !== -1) {
+      setTimeout(() => {
+        cells[bestMove].classList.add("ai-move");
+        setTimeout(() => {
+          cells[bestMove].classList.remove("ai-move");
+          spaces[bestMove] = currentPlayer;
+          cells[bestMove].textContent = currentPlayer;
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+          // Check for a win here
+          const winner = checkWinner(spaces);
+          if (winner === "O") {
+            const computerWinsText = computerScore.textContent;
+            const computerWins = parseInt(computerWinsText.match(/\d+/)[0]);
+            computerScore.textContent = `${computerWins + 1} Wins`;
+            alert("Player 2 (O) wins!");
+            resetGame();
+          }
+        }, 500);
+      }, 500);
+    }
+  }
+
+  function findBestMove(board, player) {
+    let bestScore = -Infinity;
+    let bestMove = -1;
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "") {
+        board[i] = player;
+        let score = minimax(board, 0, false);
+        board[i] = ""; // Undo the move
+
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = i;
+        }
       }
     }
-  
-    function findBestMove(board, player) {
+
+    return bestMove;
+  }
+
+  const scores = {
+    X: -10,
+    O: 10,
+    draw: 0,
+  };
+
+  function minimax(board, depth, isMaximizing) {
+    const winner = checkWinner(board);
+    if (winner !== null) {
+      return scores[winner];
+    }
+
+    if (isMaximizing) {
       let bestScore = -Infinity;
-      let bestMove = -1;
-  
       for (let i = 0; i < board.length; i++) {
-        if (board[i] === '') {
-          board[i] = player;
-          let score = minimax(board, 0, false);
-          board[i] = ''; // Undo the move
-  
-          if (score > bestScore) {
-            bestScore = score;
-            bestMove = i;
-          }
+        if (board[i] === "") {
+          board[i] = "O";
+          let score = minimax(board, depth + 1, false);
+          board[i] = ""; // Undo the move
+          bestScore = Math.max(bestScore, score);
         }
       }
-  
-      return bestMove;
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < board.length; i++) {
+        if (board[i] === "") {
+          board[i] = "X";
+          let score = minimax(board, depth + 1, true);
+          board[i] = ""; // Undo the move
+          bestScore = Math.min(bestScore, score);
+        }
+      }
+      return bestScore;
     }
-  
-    const scores = {
-      X: -10,
-      O: 10,
-      draw: 0
-    };
-  
-    function minimax(board, depth, isMaximizing) {
-      const winner = checkWinner(board);
-      if (winner !== null) {
-        return scores[winner];
-      }
-  
-      if (isMaximizing) {
-        let bestScore = -Infinity;
-        for (let i = 0; i < board.length; i++) {
-          if (board[i] === '') {
-            board[i] = 'O';
-            let score = minimax(board, depth + 1, false);
-            board[i] = ''; // Undo the move
-            bestScore = Math.max(bestScore, score);
-          }
-        }
-        return bestScore;
-      } else {
-        let bestScore = Infinity;
-        for (let i = 0; i < board.length; i++) {
-          if (board[i] === '') {
-            board[i] = 'X';
-            let score = minimax(board, depth + 1, true);
-            board[i] = ''; // Undo the move
-            bestScore = Math.min(bestScore, score);
-          }
-        }
-        return bestScore;
+  }
+
+  function checkWinner(board) {
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8], // Rows
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8], // Columns
+      [0, 4, 8],
+      [2, 4, 6], // Diagonals
+    ];
+
+    for (const combo of winningCombos) {
+      const [a, b, c] = combo;
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        return board[a];
       }
     }
-  
-    function checkWinner(board) {
-      const winningCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6] // Diagonals
-      ];
-  
-      for (const combo of winningCombos) {
-        const [a, b, c] = combo;
-        if (board[a] && board[a] === board[b] && board[b] === board[c]) {
-          return board[a];
-        }
-      }
-  
-      if (board.every(cell => cell !== '')) {
-        return 'draw';
-      }
-  
-      return null;
+
+    if (board.every((cell) => cell !== "")) {
+      return "draw";
     }
-  
-    // EventListeners for Buttons
-    document.querySelector('.reset').addEventListener('click', resetGame);
-    document.querySelector('.newGame').addEventListener('click', newGame);
-  });
-  
+
+    return null;
+  }
+
+  // EventListeners for Buttons
+  document.querySelector(".reset").addEventListener("click", resetGame);
+  document.querySelector(".newGame").addEventListener("click", newGame);
+});
